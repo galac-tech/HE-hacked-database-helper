d665ee20e317cdaa7fe40b07a53dee6c34795a82
galac-tech <galac-tech@users.noreply.github.com>
2016-06-13 16:47:20 -0700

    Update HD_Helper.js

--- HD_Helper.js
+++ HD_Helper.js
@@ -1,4 +1,3 @@
-//just a test comment!
 var IP_pages = [document.getElementById("list").innerHTML];
 var footer = document.getElementsByClassName("pagination alternate")[0];
 var liTags = footer.getElementsByTagName("li");
@@ -66,25 +65,46 @@ function loadAll() {
         }
         bodyDiv.innerHTML = '<form class="form-horizontal">' +
             '<div class="browser-input">' +
-            'Internet Speeds : <select id="internetSpeed" style="margin-right: 10px;">' +
+            'Sort by : <select id="sortType" style="margin-right: 10px;">' +
             '<option class="form" value="all">View all</option>' +
-            '<option class="form" value="1">1 Mbit/s</option>' +
-            '<option class="form" value="2">2 Mbit/s</option>' +
-            '<option class="form" value="4">4 Mbit/s</option>' +
-            '<option class="form" value="6">6 Mbit/s</option>' +
-            '<option class="form" value="10">10 Mbit/s</option>' +
-            '<option class="form" value="25">25 Mbit/s</option>' +
-            '<option class="form" value="50">50 Mbit/s</option>' +
-            '<option class="form" value="100">100 Mbit/s</option>' +
-            '<option class="form" value="250">250 Mbit/s</option>' +
-            '<option class="form" value="500">500 Mbit/s</option>' +
-            '<option class="form" value="1000">1 Gbit/s (1000 Mbit/s)</option>' +
+            '<option class="form" value="internetSpeeds">Internet Speeds</option>' +
+            '<option class="form" disabled>More comming soon!</option>' +
             '</select>' +
-            '<input type="button" class="btn btn-inverse" id="searchIPdat" value="Search">' +
             '</div>' +
             '</form>';
-        document.getElementById("searchIPdat").addEventListener("click", searchIPdat);
         savedDat = document.getElementById("list").cloneNode(true);
+        $('#sortType').change(function() {
+            var value = $(this).val();
+            if (value == "internetSpeeds") {
+                var div = document.getElementById("hidden-div");
+                var form = document.createElement("FORM");
+                form.className = "form-horizontal";
+                form.innerHTML = '<div id="internetSpeed-div"class="browser-input">' +
+                    'Internet Speeds : <select id="internetSpeed" style="margin-right: 10px;">' +
+                    '<option class="form" value="selectOne">Select one</option>' +
+                    '<option class="form" value="1">1 Mbit/s</option>' +
+                    '<option class="form" value="2">2 Mbit/s</option>' +
+                    '<option class="form" value="4">4 Mbit/s</option>' +
+                    '<option class="form" value="10">10 Mbit/s</option>' +
+                    '<option class="form" value="25">25 Mbit/s</option>' +
+                    '<option class="form" value="50">50 Mbit/s</option>' +
+                    '<option class="form" value="100">100 Mbit/s</option>' +
+                    '<option class="form" value="250">250 Mbit/s</option>' +
+                    '<option class="form" value="500">500 Mbit/s</option>' +
+                    '<option class="form" value="1000">1 Gbit/s (1000 Mbit/s)</option>' +
+                    '<option class="form" value="other">Other</option>' +
+                    '</select>' +
+                    '</div>';
+                div.appendChild(form);
+                $('#internetSpeed').change(function() {
+                    searchIPdat();
+                });
+
+            } else if (value == "all") {
+                document.getElementById("internetSpeed-div").remove();
+                document.getElementById("list").innerHTML = savedDat.innerHTML;
+            }
+        });
         checkFavorites();
         $.getScript("https://legacy.hackerexperience.com/js/main.js.pagespeed.jm.oC0Po-3w4s.js", function() {});
     }, (numberOfPages * 0.08) * 1000);
@@ -151,13 +171,30 @@ $(window).scroll(function() {
 });
 
 function searchIPdat() {
-    speed = document.getElementById("internetSpeed").value;
-    if (speed == "1000") {
+    var speed = document.getElementById("internetSpeed").value;
+    if (speed == "selectOne") {
+        return;
+    } else if (speed == "1000") {
         speed = "1 Gbit/s";
-    } else if (speed == "all") {
-        document.getElementById("list").innerHTML = savedDat.innerHTML;
+    } else if (speed == "other") {
+        var normalSpeeds = ["1 Mbit/s", "2 Mbit/s", "4 Mbit/s", "10 Mbit/s", "25 Mbit/s", "50 Mbit/s", "100 Mbit/s", "250 Mbit/s", "500 Mbit/s", "1 Gbit/s"];
+        lis = savedDat.children;
+        var group = [];
+        for (var i in lis) {
+            try {
+                foundSpeed = lis[i].children[2].children[0].children[0].innerText;
+                if (ifIn(foundSpeed, normalSpeeds) === false) {
+                    if (ifIn('<li id="' + lis[i].id + '" >' + lis[i].innerHTML + "</li>", group) === false) {
+                        group.push('<li id="' + lis[i].id + '" >' + lis[i].innerHTML + "</li>");
+                    }
+                }
+            } catch (err) {
+                continue;
+            }
+        }
+        document.getElementById("list").innerHTML = group.join("\n");
         checkFavorites();
-        $.getScript("https://legacy.hackerexperience.com/js/main.js.pagespeed.jm.oC0Po-3w4s.js", function(){});
+        $.getScript("https://legacy.hackerexperience.com/js/main.js.pagespeed.jm.oC0Po-3w4s.js", function() {});
         return;
     } else {
         speed = speed + " Mbit/s";
@@ -166,9 +203,12 @@ function searchIPdat() {
     var group = [];
     for (var i in lis) {
         try {
-            foundSpeed = lis[i].children[2].children[0].children[0].innerText;
+            var foundSpeed = lis[i].children[2].children[0].children[0].innerText;
+
             if (foundSpeed == speed) {
+
                 if (ifIn('<li id="' + lis[i].id + '" >' + lis[i].innerHTML + "</li>", group) === false) {
+
                     group.push('<li id="' + lis[i].id + '" >' + lis[i].innerHTML + "</li>");
                 }
             }
@@ -197,13 +237,13 @@ $(document).ready(function() {
         setTimeout(function() {
             if (isEven(times) === false) {
                 $("#animatedElm").animate({
-                    marginTop: '+=50px'
+                    marginTop: '+=3px'
                 }, 500);
                 $("#hidden-div").show(500);
                 document.getElementById("hidden-div").style.display = "block";
             } else {
                 $("#animatedElm").animate({
-                    marginTop: '-=50px'
+                    marginTop: '-=3px'
                 }, 500);
                 $("#hidden-div").hide(500);
                 document.getElementById("hidden-div").style.display = "none";
