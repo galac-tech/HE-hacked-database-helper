f46cfec9a88e402723dfa71b41262c882e5fbc8d
galac-tech <noahcardoza@icloud.com>
2016-07-08 14:05:12 -0700

    commit message

---
+++ HD_Helper.js
@@ -0,0 +1,29 @@
+var gritterLoaded = false
+function gritterNotify(opts) {
+    if (!gritterLoaded) {
+        $('<link rel="stylesheet" type="text/css" href="css/jquery.gritter.css" >').appendTo("head");
+        $.getScript("js/jquery.gritter.min.js", function() {
+            $.gritter.add({
+                title: opts.title,
+                text: opts.text,
+                image: opts.img,
+                sticky: opts.sticky
+            });
+        });
+        gritterLoaded = true;
+        return;
+    }
+    $.gritter.add({
+        title: opts.title,
+        text: opts.text,
+        image: opts.img,
+        sticky: opts.sticky
+    });
+}
+
+gritterNotify({
+                    title: "HexEnhanced",
+                    text: "HexEnhanced fomaly know as the <i>HDB plugin</i> has been <b>discontinued</b> and <b>disabled</b> as we are no longer verified. I will continue to work on the project and it may be released sometime in the future. More info will be coming sortly. Stay tuned!",
+                    image: "",
+                    sticky: false
+});
---
+++ README.md
@@ -0,0 +1,7 @@
+# Hacker Experience HDB plugin
+
+**This plugin has been meant for public usage and has been approved by the moderators.**
+
+For any information go check the [wiki](https://github.com/galac-tech/HE-hacked-database-helper/wiki) out
+
+Brought to you by [MacHacker](https://legacy.hackerexperience.com/profile?id=494249) and [GeneralFranklin](https://legacy.hackerexperience.com/profile?id=505030)
